f7cd08095026bf538eaff1832ddf66b1599397e1
vikasparyani <vikas.paryani98@gmail.com>
2021-08-17 22:30:14 +0530

    First commit

---
+++ RateLimiter/RateLimiter/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RateLimiter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var limiter = new RateLimiterClass(10, TimeSpan.FromSeconds(5));
+
+            var t = Task.Run(() =>
+            {
+                for (var i = 0; i < 100; i++)
+                {
+                    if(!limiter.Process("App1"))
+                    {
+                        //Task.Delay(1).Wait();
+                        Console.WriteLine("App1 - Rejected");
+                        break;
+                    } else
+                    {
+                        Console.WriteLine("App1 - Accepted");
+                    }
+                }
+            });
+
+           
+
+            var t1 = Task.Run(() =>
+            {
+                for (var i = 0; i < 100; i++)
+                {
+                    if (!limiter.Process("App2"))
+                    {
+                        //Task.Delay(1).Wait();
+                        Console.WriteLine("App2 - Rejected");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("App2 - Accepted");
+                    }
+                }
+            });
+
+            Task.WaitAll(t,t1);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RateLimiter/RateLimiter/RateLimiterClass.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading;
+
+namespace RateLimiter
+{
+    [Serializable()]
+    public class RateLimiterClass
+    {
+        public RateLimiterClass(Int32 requests, TimeSpan interval)
+        {
+            this.Requests = requests;
+            this.Interval = interval;
+        }
+
+        public Int32 Requests { get; set; }
+        public TimeSpan Interval { get; set; }
+
+        private ReaderWriterLockSlim _lock = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
+        private Dictionary<String, Queue<DateTime>> _internalStorage = new Dictionary<String, Queue<DateTime>>();
+
+        public Boolean Process(String address)
+        {
+            _lock.EnterUpgradeableReadLock();
+            try
+            {
+                if (!_internalStorage.ContainsKey(address))
+                {
+                    _lock.EnterWriteLock();
+                    try
+                    {
+                        _internalStorage.Add(address, new Queue<DateTime>());
+                        _internalStorage[address].Enqueue(DateTime.Now);
+                        return true;
+                    }
+                    finally
+                    {
+                        _lock.ExitWriteLock();
+                    }
+                }
+                else
+                {
+                    PurgeOldEntries(address);
+
+                    _lock.EnterWriteLock();
+                    try
+                    {
+                        _internalStorage[address].Enqueue(DateTime.Now);
+                        return _internalStorage[address].Count <= Requests;
+                    }
+                    finally
+                    {
+                        _lock.ExitWriteLock();
+                    }
+                }
+            }
+            finally
+            {
+                _lock.ExitUpgradeableReadLock();
+            }
+        }
+
+        private void PurgeOldEntries(String address)
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                while (_internalStorage[address].Count > 0 && (DateTime.Now - _internalStorage[address].Peek() > Interval))
+                {
+                    _internalStorage[address].Dequeue();
+                }
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
+    }
+}
